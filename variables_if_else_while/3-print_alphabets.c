d988e44e1bb216136be98a5395c672a65eacf9d8
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2024-10-21 05:10:41 +0000

    oui

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - main function
+*
+* Return: 0 for success
+*/
+int main(void)
+{
+	char *alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
+	int i = 0;
+
+	while (i != 53)
+	}
+		putchar(alphabet[i]);
+		 i++;
+	}
+	return (0);
+}
