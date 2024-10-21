3a97308957dd5280e806b22eee71d74ac95f044a
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-21 11:33:17 +0200

    monkey

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,16 +5,16 @@
 */
 void print_alphabet_x10(void)
 {
-char *alphabet = "abcdefghijklmnopqrstuvwxyz";
-int i;
-int j;
+    char *alphabet = "abcdefghijklmnopqrstuvwxyz";
+    int i;
+    int j;
 
-for (j = 0; j != 10; j++)
-{
-for (i = 0; i < 26; i++)
-{
-_putchar(alphabet[i]);
-}
-_putchar('\n');
-}
+    for (j = 0; j != 10; j++)
+    {
+        for (i = 0; i < 26; i++)
+        {
+            _putchar(alphabet[i]);
+        }
+        _putchar('\n');
+    }
 }
