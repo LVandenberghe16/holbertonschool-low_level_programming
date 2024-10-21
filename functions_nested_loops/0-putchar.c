8a201070fded0f39c2be5aba5993e0049a76b68f
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-21 09:06:57 +0200

    oui

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,12 +8,7 @@
 int main(void)
 {
 	char *string = "_putchar";
-	int i;
 
-	for (i = 0; i != strlen(string); i++)
-	{
-		write(0, string[i], 1);
-	}
-	write(0, '\n', 1);
+	write(1, string, strlen(string));
 	return (0);
 }
