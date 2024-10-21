f2e5d429ef0195813aeb1c39151aa246c9e0edf8
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-21 12:10:49 +0200

    seal

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 int count;
-unsigned long fib1 = 1, fib2 = 2, next_fib;
+unsigned long long fib1 = 1, fib2 = 2, next_fib;
 
 printf("%lu, %lu", fib1, fib2);
 for (count = 3; count <= 98; count++)
