890cb1377826fa6ca6f9135854cbbcf6851fee3a
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-21 09:58:53 +0200

    elephant

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _islower - _islower function
+* @c: the string you want to get the size of
+*
+* Description: this function tell if the case is lower
+*
+* Return: return lenght of a string
+*/
+int _islower(int c)
+{
+    if (c >= 'a' && c <= 'z')
+        return (1);
+    else
+        return (0);
+}
