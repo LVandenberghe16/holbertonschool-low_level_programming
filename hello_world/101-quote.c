b3fdd83e34ce6d52614170ddc50fcf50fa6d362f
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-21 11:05:52 +0200

    pigeon

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-const char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+const char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 write(2, quote, 61);
 return 1;
 }
