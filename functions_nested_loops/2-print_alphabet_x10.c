64fbe9a28845bb3579084093aa3ffd5974e0e3d0
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-21 09:50:38 +0200

    oui

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints the alphabet in lowercase, followed by a new line.
+*/
+void print_alphabet_x10(void)
+{
+    char *alphabet = "abcdefghijklmnopqrstuvwxyz";
+    int i;
+    int j;
+
+    for (j = 0; j != 10; j++)
+    {
+        for (i = 0; i < 26; i++)
+        {
+            _putchar(alphabet[i]);
+        }
+        _putchar('\n');
+    }
+}
