962f0284121589ae5ad94a533ed0b15020966e53
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-21 09:13:01 +0200

    oui

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,11 +9,13 @@ int my_strlen(char const *str)
 {
     int i = 0;
     int len = 0;
-    while (str[i] != '\0'){
+
+    while (str[i] != '\0')
+	{
         len += 1;
         i++;
     }
-    return len;
+    return (len);
 }
 
 /**
