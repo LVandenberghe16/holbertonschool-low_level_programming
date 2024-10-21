bbcd75c02b6e0c344551ae8f38d915347787bec1
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-21 09:09:22 +0200

    oui

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+* my_strlen - my_strlen function
+*
+* Return: return lenght of a string
+*/
+int my_strlen(char const *str)
+{
+    int i = 0;
+    int len = 0;
+    while (str[i] != '\0'){
+        len += 1;
+        i++;
+    }
+    return len;
+}
+
+/**
 * main - main function
 *
 * Return: return success
@@ -9,6 +25,6 @@ int main(void)
 {
 	char *string = "_putchar";
 
-	write(1, string, strlen(string));
+	write(1, string, my_strlen(string));
 	return (0);
 }
