732cdc3241720d1217c337cc07f56395d2a87397
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2024-10-21 04:39:21 +0000

    add

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - main function of the program
+*
+* Return: 0 for no error
+*/
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,",);
+        return (0);
+}
