496f6f22421d03e6e6479fb5d7e0b9fd8f8b9ed7
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2024-10-21 04:34:33 +0000

    add

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-/*  main function of the program */
+/**
+*  main function of the program
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
