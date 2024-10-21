d94f955450cfc1f5466f9dcde1aad613ed176988
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-21 09:28:42 +0200

    add

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,5 +1,43 @@
 #include "main.h"
 
+/**
+* my_strlen - my_strlen function
+* @str: the string you want to get the size of
+*
+* Description: this function return the size of a string
+*
+* Return: return lenght of a string
+*/
+int my_strlen(char const *str)
+{
+	int i = 0;
+	int len = 0;
+
+	while (str[i] != '\0')
+	{
+		len += 1;
+		i++;
+	}
+	return (len);
+}
+
+/**
+* _putchar - _putchar function
+*
+* @string: the string you want to print
+*
+* Return: return success
+*/
+int _putchar(char *string)
+{
+	write(1, string, my_strlen(string));
+	return (0);
+}
+
+/**
+ * print_alphabet - check the code
+ *
+ */
 void print_alphabet(void)
 {
     _putchar("abcdefghijklmnopqrstuvwxyz\n");
