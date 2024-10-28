ebc9c825e77211a7edd71816365c8a3c805cc641
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-28 10:21:26 +0100

    oui

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -31,7 +31,7 @@ static int overflow(int nb, int i, char const *str)
     return 0;
 }
 
-int my_getnbr(char const *str)
+int _atoi(char *s)
 {
     int nb = 0;
     char sign;
@@ -39,17 +39,17 @@ int my_getnbr(char const *str)
     int i;
 
 
-    for (i = 0 ; str[i] != '\0' ; i += 1) {
-        sign = is_sign(str[i]);
-        if (my_isnum(str[i]) == 0 && (sign == 0 || nb != 0))
+    for (i = 0 ; s[i] != '\0' ; i += 1) {
+        sign = is_sign(s[i]);
+        if (my_isnum(s[i]) == 0 && (sign == 0 || nb != 0))
             return is_odd(minus) == 1 ? -nb : nb;
         if (sign == 1)
             minus += 1;
-        if (my_isnum(str[i]) == 1 && overflow(nb, i, str) == 1)
+        if (my_isnum(s[i]) == 1 && overflow(nb, i, s) == 1)
             return 0;
-        if (my_isnum(str[i]) == 1) {
+        if (my_isnum(s[i]) == 1) {
             nb = nb * 10;
-            nb += str[i] - 48;
+            nb += s[i] - 48;
         }
 
     }
