ede204cbf382c129b2ead972fbcd0e11dda6fd57
LVandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-28 01:38:08 +0100

    add readme

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,17 +1,28 @@
 #include <unistd.h>
-
+/**
+* print_diagonal - oui
+* @n: num
+* Return: restult
+*/
 void print_diagonal(int n)
 {
+	int i, j;
 	if (n <= 0)
 		return (void)write(1, "\n", 1);
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < i; j++) {
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
 			write(1, " ", 1);
 		}
 		write(1, "\\\n", 2);
 	}
 }
 
+/**
+* main - oui
+* Return: restult
+*/
 int main(void)
 {
     print_diagonal(0);
