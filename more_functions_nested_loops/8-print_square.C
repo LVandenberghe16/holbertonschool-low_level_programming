342c2ef1665e7a187a76d43e67b5ebad4cb96ea7
LVandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-28 01:42:57 +0100

    add readme

--- more_functions_nested_loops/8-print_square.C
+++ more_functions_nested_loops/8-print_square.C
@@ -1,21 +1,25 @@
 #include <unistd.h>
 
+/**
+* print_square - oui
+* @n: num
+* Return: restult
+*/
 void print_square(int n)
 {
+	int i, j;
+
 	if (n <= 0)
-		return (void)write(1, "\n", 1);
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
+	{
+		write(1, "\n", 1);
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
 			write(1, "#", 1);
 		}
 		write(1, "\n", 1);
 	}
-}
-
-int main(void)
-{
-    print_square(0);
-    print_square(2);
-    print_square(10);
-    return (0);
 }
