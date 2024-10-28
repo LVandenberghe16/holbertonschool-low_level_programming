1755374cb6289432b2099364109fed2cbaba8478
LVandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-28 01:54:02 +0100

    add readme

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,26 +1,30 @@
+#include <unistd.h>
+
+/**
+* print_triangle - oui
+* @size: num
+* Return: restult
+*/
+
 void print_triangle(int size)
 {
+	int i, j;
+
 	if (size <= 0)
-		return (void) write(1, "\n", 1);
-    	for (int i = 0; i < size; i++)
 	{
-		for (int j = 0; j < (size - i); j++)
+		write(1, "\n", 1);
+		return;
+	}
+    for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size - i); j++)
 		{
 			write(1, " ", 1);
 		}
-		for (int j = 0; j < i; j++)
+		for (j = 0; j < i; j++)
 		{
 			write(1, "#", 1);
 		}
 	write(1, "\n", 1);
 	}
-}
-
-int main(void)
-{
-    print_triangle(2);
-    print_triangle(10);
-    print_triangle(1);
-    print_triangle(0);
-    return (0);
 }
