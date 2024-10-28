8643bab880140a49c12100d26c79bd24fec44431
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-28 09:48:38 +0100

    oui

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,18 +6,11 @@
  */
 void puts2(char *str)
 {
-    int i = 0;
+    int i;
 
-    while (str[i] != '\0')
+    for (i = 0; str[i] != '\0'; i += 2)
     {
-		if (i > 220 && str[i] == '.')
-		{
-			_putchar(str[i]);
-			goto end;
-		}
         _putchar(str[i]);
-        i += 2;
     }
-end:
     _putchar('\n');
 }
