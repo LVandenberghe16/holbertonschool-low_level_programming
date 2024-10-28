3fe680b1aaa41723949a2ff520da22d810d9192c
LVandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-28 01:32:06 +0100

    add readme

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,14 @@
+#include <unistd.h>
+
+/**
+* more_numbers - oui
+*
+* Return: restult
+*/
 void more_numbers(void)
 {
 	for (int i = 0; i < 10; i++)
-{
-write(1, "01234567891011121314\n", 21);
-}
+	{
+		write(1, "01234567891011121314\n", 21);
+	}
 }
