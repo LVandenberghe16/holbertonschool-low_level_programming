f6e3c8e510ffea56267051061a7cd13bfdc6f679
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-28 10:16:44 +0100

    oui

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: Pointer to the string to convert.
+ *
+ * Return: The converted integer, or 0 if there are no numbers.
+ */
+int _atoi(char *s)
+{
+    int sign = 1;
+    unsigned int num = 0;
+    int i = 0;
+
+    while (s[i] == ' ')
+        i++;
+    while (s[i] == '-' || s[i] == '+')
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        i++;
+    }
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        if (num > (2147483647 - (s[i] - '0')) / 10)
+            return (sign == 1) ? 2147483647 : -2147483648;
+        num = num * 10 + (s[i] - '0');
+        i++;
+    }
+    return (num * sign);
+}
