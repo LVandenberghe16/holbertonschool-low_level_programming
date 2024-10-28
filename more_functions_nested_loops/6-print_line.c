9e20a5cb834053f4350cfece1668756568808bf4
LVandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-28 01:34:16 +0100

    add readme

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,20 @@
+#include <unistd.h>
+
+/**
+* mul - oui
+* @n: num
+* Return: restult
+*/
 void print_line(int n)
 {
+	int i;
+
 	if (n <= 0) {
 		goto newline;
 	}
-	for (int i = 0; i < n; i++) {
+	for (i = 0; i < n; i++) {
 		write(1, "_", 1);
 	}
-newline:
-	write(1, "\n", 1);
+	newline:
+		write(1, "\n", 1);
 }
