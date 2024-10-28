579a5d1b9af0ff79b15d2ff1fb12df6cdf657c37
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-28 10:26:47 +0100

    oui

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,10 +11,10 @@ int _atoi(char *s)
     int sign = 1;
     unsigned int num = 0;
     int i = 0;
+    int found_digit = 0;
 
     while (s[i] == ' ')
         i++;
-
     while (s[i] == '-' || s[i] == '+')
     {
         if (s[i] == '-')
@@ -23,13 +23,13 @@ int _atoi(char *s)
     }
     while (s[i] >= '0' && s[i] <= '9')
     {
-        if (num > (unsigned int)(2147483647 / 10) ||
-            (num == (unsigned int)(2147483647 / 10) && (s[i] - '0') > 7))
-        {
+        found_digit = 1;
+        if (num > (2147483647 / 10) || (num == (2147483647 / 10) && (s[i] - '0') > 7))
             return (sign == 1) ? 2147483647 : -2147483648;
-        }
         num = num * 10 + (s[i] - '0');
         i++;
     }
+    if (!found_digit)
+        return 0;
     return (sign == -1) ? -num : num;
 }
