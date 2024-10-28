025880d537ae420ef0ee467d28335ddfd4b9aaba
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-28 10:27:38 +0100

    oui

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,28 +8,43 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    unsigned int num = 0;
-    int i = 0;
-    int found_digit = 0;
+    int sign = 1; // Variable to hold the sign of the number
+    unsigned int num = 0; // Variable to accumulate the number
+    int i = 0; // Index for traversing the string
+    int found_digit = 0; // Flag to check if we've found any digits
 
+    // Skip whitespace characters
     while (s[i] == ' ')
+    {
         i++;
+    }
+
+    // Check for sign characters
     while (s[i] == '-' || s[i] == '+')
     {
         if (s[i] == '-')
-            sign *= -1;
+            sign *= -1; // Change the sign if a '-' is found
         i++;
     }
+
+    // Convert the number part of the string
     while (s[i] >= '0' && s[i] <= '9')
     {
-        found_digit = 1;
+        found_digit = 1; // Mark that we've found a digit
+        // Prevent integer overflow
         if (num > (2147483647 / 10) || (num == (2147483647 / 10) && (s[i] - '0') > 7))
-            return (sign == 1) ? 2147483647 : -2147483648;
-        num = num * 10 + (s[i] - '0');
+        {
+            return (sign == 1) ? 2147483647 : -2147483648; // Handle overflow case
+        }
+        num = num * 10 + (s[i] - '0'); // Build the number
         i++;
     }
+
+    // If no digits were found, return 0
     if (!found_digit)
+    {
         return 0;
-    return (sign == -1) ? -num : num;
+    }
+
+    return (sign == -1) ? -num : num; // Apply the sign to the result
 }
