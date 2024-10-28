d346e05a566d00957f799eea4820aa9f13afa2ef
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-28 10:22:09 +0100

    oui

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,57 +1,31 @@
 #include "main.h"
 
-static int is_sign(char c)
-{
-    if (c == '-')
-        return 1;
-    if (c == '+')
-        return 2;
-    return 0;
-}
-
-static int is_odd(int nb)
-{
-    if (nb % 2 == 0)
-        return 0;
-    return 1;
-}
-
-static int my_isnum(char c)
-{
-    if (47 < c && c < 58)
-        return (1);
-    return (0);
-}
-
-static int overflow(int nb, int i, char const *str)
-{
-    if ((str[i] > '7' && nb == 214748364) || nb > 214748364 ) {
-        return 1;
-    }
-    return 0;
-}
-
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: Pointer to the string to convert.
+ *
+ * Return: The converted integer, or 0 if there are no numbers.
+ */
 int _atoi(char *s)
 {
-    int nb = 0;
-    char sign;
-    int minus = 0;
-    int i;
+    int sign = 1;
+    unsigned int num = 0;
+    int i = 0;
 
-
-    for (i = 0 ; s[i] != '\0' ; i += 1) {
-        sign = is_sign(s[i]);
-        if (my_isnum(s[i]) == 0 && (sign == 0 || nb != 0))
-            return is_odd(minus) == 1 ? -nb : nb;
-        if (sign == 1)
-            minus += 1;
-        if (my_isnum(s[i]) == 1 && overflow(nb, i, s) == 1)
-            return 0;
-        if (my_isnum(s[i]) == 1) {
-            nb = nb * 10;
-            nb += s[i] - 48;
-        }
-
+    while (s[i] == ' ')
+        i++;
+    while (s[i] == '-' || s[i] == '+')
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        i++;
     }
-    return is_odd(minus) == 1 ? -nb : nb;
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        if (num > (2147483647 - (s[i] - '0')) / 10)
+            return (sign == 1) ? 2147483647 : -2147483648;
+        num = num * 10 + (s[i] - '0');
+        i++;
+    }
+    return (num * sign);
 }
