6ad6aa0a40862f9bc3940e4b8cd83510cf584188
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-28 10:20:03 +0100

    oui

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,31 +1,55 @@
 #include "main.h"
 
-/**
- * _atoi - Converts a string to an integer.
- * @s: Pointer to the string to convert.
- *
- * Return: The converted integer, or 0 if there are no numbers.
- */
-int _atoi(char *s)
+static int is_sign(char c)
 {
-    int sign = 1;
-    int num = 0;
-    int i = 0;
+    if (c == '-')
+        return 1;
+    if (c == '+')
+        return 2;
+    return 0;
+}
 
-    while (s[i] == ' ')
-        i++;
-    while (s[i] == '-' || s[i] == '+')
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        i++;
+static int is_odd(int nb)
+{
+    if (nb % 2 == 0)
+        return 0;
+    return 1;
+}
+
+static int my_isnum(char c)
+{
+    if (47 < c && c < 58)
+        return (1);
+    return (0);
+}
+
+static int overflow(int nb, int i, char const *str)
+{
+    if ((str[i] > '7' && nb == 214748364) || nb > 214748364 ) {
+        return 1;
     }
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        if (num > (2147483647 - (s[i] - '0')) / 10)
-            return (sign == 1) ? 2147483647 : -2147483648;
-        num = num * 10 + (s[i] - '0');
-        i++;
+    return 0;
+}
+
+int my_getnbr(char const *str)
+{
+    int nb = 0;
+    char sign;
+    int minus = 0;
+
+    for (int i = 0 ; str[i] != '\0' ; i += 1) {
+        sign = is_sign(str[i]);
+        if (my_isnum(str[i]) == 0 && (sign == 0 || nb != 0))
+            return is_odd(minus) == 1 ? -nb : nb;
+        if (sign == 1)
+            minus += 1;
+        if (my_isnum(str[i]) == 1 && overflow(nb, i, str) == 1)
+            return 0;
+        if (my_isnum(str[i]) == 1) {
+            nb = nb * 10;
+            nb += str[i] - 48;
+        }
+
     }
-    return (num * sign);
+    return is_odd(minus) == 1 ? -nb : nb;
 }
