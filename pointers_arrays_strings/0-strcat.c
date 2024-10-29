bfccc961dadd33c3aeeb7735220b339e86c95e07
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-10-29 09:14:23 +0100

    oui

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates the src string to the dest string
+ * @dest: The destination string to which src will be appended
+ * @src: The source string to be appended to dest
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+    int dest_len = 0;
+    int i;
+
+    while (dest[dest_len] != '\0')
+        dest_len++;
+    for (i = 0; src[i] != '\0'; i++)
+        dest[dest_len + i] = src[i];
+    dest[dest_len + i] = '\0';
+    return dest;
+}
