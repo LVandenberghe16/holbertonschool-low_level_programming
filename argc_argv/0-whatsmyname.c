6fe844491fc462e5d9432f0fe4a713b7cdb4362b
LVandenberghe <ludwig.vandenberghe@epitech.eu>
2024-11-04 20:04:21 +0100

    oui

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program's name followed by a new line
+ * @ac: the argument count
+ * @av: an array of strings representing the arguments passed to the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int ac, char **av)
+{
+	printf("%s\n", av[0]);
+	return (0);
+}
