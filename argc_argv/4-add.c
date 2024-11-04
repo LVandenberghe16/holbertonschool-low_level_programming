26e9940661b595bc82f45350f7fcb8c12013d9fe
LVandenberghe <ludwig.vandenberghe@epitech.eu>
2024-11-04 20:39:02 +0100

    oui

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * is_digit - prints the program's name followed by a new line
+ * @str: the argument count
+ *
+ * Return: Always 0 (Success)
+ */
+int is_digit(char *str)
+{
+	if (*str == '\0')
+		return (0);
+	if (*str == '+' || *str == '-')
+		str++;
+	while (*str)
+	{
+		if (!isdigit((unsigned char)*str))
+			return (0);
+		str++;
+	}
+	return (1);
+}
 
 /**
  * main - prints the program's name followed by a new line
@@ -18,8 +40,15 @@ int main(int ac, char **av)
 		printf("0");
 		return (0);
 	}
-	for (i = 0; i != ac; i++)
+	for (i = 1; i != ac; i++)
+	{
+		if (is_digit(av[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
 		sum = sum + atoi(av[i]);
+	}
 	printf("%i\n", sum);
 	return (0);
 }
