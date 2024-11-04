a206e9828348c09a12cc6c098d57f09446e0d473
Ludwig Vandenberghe <ludwig.vandenberghe@epitech.eu>
2024-11-04 09:56:19 +0100

    oui

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,5 +11,24 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+	char *n;
+	char *h;
 
+	if (*needle == '\0')
+		return (haystack);
+	while (*haystack)
+	{
+		n = needle;
+		h = haystack;
+		while (*n && (*h == *n))
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+			return (haystack);
+		haystack++;
+	}
+	return (NULL);
 }
+
