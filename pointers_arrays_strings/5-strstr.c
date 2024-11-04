6aa18839d2ab34c0429c40ee0a5cb2f5ad3b3f01
LVandenberghe <ludwig.vandenberghe@epitech.eu>
2024-11-04 07:26:59 +0100

    oui

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - fills the first n bytes of the memory area
+ *           pointed to by s with the constant byte b.
+ * @haystack: pointer to the memory area to fill
+ * @needle: constant byte to fill memory with
+ *
+ * Return: pointer to the memory area s
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+}
