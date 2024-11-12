f7543d73681b1d3f8946a4351e8d396910466a3d
LVandenberghe <ludwig.vandenberghe@epitech.eu>
2024-11-12 18:39:04 +0100

    oui

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,6 +16,7 @@ struct dog
 	float age; /* age of the dog*/
 	char *owner; /* owner of the dog*/
 };
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 char *_strdup(char *str);
