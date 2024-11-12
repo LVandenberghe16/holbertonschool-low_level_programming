3f20160e5d1bdcc26c2d65710f331b45c7d1cb27
LVandenberghe <ludwig.vandenberghe@epitech.eu>
2024-11-12 19:08:20 +0100

    oui

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * free_dog - Allocates memory using malloc and checks for success.
+ * @d: Number of bytes to allocate.
+ *
+ * Return: Pointer to the allocated memory. If malloc fails, the function
+ *         causes normal process termination with a status value of 98.
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d);
+	}
+}
