229b2a2d669b1c11963c451164441d504d1e4b50
LVandenberghe <ludwig.vandenberghe@epitech.eu>
2024-11-12 19:04:37 +0100

    oui

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,30 @@
 #include "dog.h"
-#include <string.h>
+
+/**
+ * _strdup - duplicates a string in a newly allocated memory space
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string, or NULL if insufficient memory
+ *         or if str is NULL
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int i, length = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[length] != '\0')
+		length++;
+	dup = (char *)malloc((length + 1) * sizeof(char));
+	if (dup == NULL)
+		return (NULL);
+	for (i = 0; i < length; i++)
+		dup[i] = str[i];
+	dup[length] = '\0';
+	return (dup);
+}
+
 
 /**
  * new_dog - Allocates memory using malloc and checks for success.
@@ -17,13 +42,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = strdup(name);
+	dog->name = _strdup(name);
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->owner = strdup(owner);
+	dog->owner = _strdup(owner);
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
