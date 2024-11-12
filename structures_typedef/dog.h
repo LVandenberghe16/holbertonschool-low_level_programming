4aac1df4881f8eaf0680e49c5392ecd17bb7dceb
LVandenberghe <ludwig.vandenberghe@epitech.eu>
2024-11-12 18:19:48 +0100

    oui

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,16 +2,16 @@
 #define MAIN_H
 
 /**
- * dog - a structure representing a point in 2D space
+ * struct dog - a structure representing a point in 2D space
  * @name: the x coordinate of the point
  * @age: the y coordinate of the point
  * @owner: the y coordinate of the point
  */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+	char *name; /* name of the dog*/
+	float age; /* age of the dog*/
+	char *owner; /* owner of the dog*/
 };
 
 #endif /* MAIN_H */
