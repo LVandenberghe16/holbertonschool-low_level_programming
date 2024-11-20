0d5ae192d54da87cf067a386403701168c6fa31c
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2024-11-20 10:33:23 +0100

    oui

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -7,13 +7,12 @@
  *
  * Return: the sum of all parameters, or 0 if n == 0
  */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
 
-	if (n == 0)
-		return;
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
