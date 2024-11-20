5c9ce498bcce257e96e6ac98e3bc7ee58089a16e
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2024-11-20 10:44:56 +0100

    oui

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -12,12 +12,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
+	char *str;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		if (args != NULL)
-			printf("%s", va_arg(args, char *));
+		str = va_arg(args, char *);
+		if (str != NULL)
+			printf("%s", str);
 		else
 			printf("(nil)");
 		if (separator != NULL && i != n - 1)
