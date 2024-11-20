ab041c3792fba4040e8d6a1178073bab2e1f5c77
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2024-11-20 11:16:18 +0100

    oui

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,46 @@
+#include "variadic_functions.h"
+#include <string.h>
+
+/**
+ * print_all - returns the sum of all its parameters
+ *  @: oui
+ *  @format: the number of parameters
+ *
+ * Return: the sum of all parameters, or 0 if n == 0
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	char *str;
+	unsigned int len = strlen(format);
+	int i = 0;
+
+	va_start(args, format);
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+		case 's':
+			str = va_arg(args, char *);
+			if (str == NULL)
+				printf("(nil),");
+			else
+				printf("%s,", str);
+			break;
+		case 'f':
+			printf("%f,", va_arg(args, double));
+			break;
+		case 'c':
+			printf("%c,", va_arg(args, int));
+			break;
+		case 'i':
+			printf("%i,", va_arg(args, int));
+			break;
+		default:
+			break;
+		}
+		i++;
+	}
+	printf("\b \n");
+}
