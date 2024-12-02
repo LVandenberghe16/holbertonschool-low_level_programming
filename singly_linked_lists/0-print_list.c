63182dad5133f941cc05cc0b7a987213e3e04fc6
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2024-12-02 12:51:22 +0100

    tried without printf

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,20 +1,65 @@
-#include <stdio.h>
+#include <unistd.h>
 #include "lists.h"
 
 /**
- * print_list - Prints all elements of a list_t list.
- * @h: Pointer to the first node of the list.
+ * _putchar - writes a character to stdout.
+ * @c: The character to print.
  *
- * Return: The total number of nodes in the list.
+ * Return: On success, 1. On error, -1 is returned
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _puts - Prints a string followed by a new line to stdout.
+ * @str: The string to print.
+ */
+void _puts(const char *str)
+{
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
+}
+
+/**
+ * print_number - Prints an unsigned integer to stdout.
+ * @n: The number to print.
+ */
+void print_number(unsigned int n)
+{
+	if (n / 10)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
+}
+
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_list(const list_t *h)
 {
-	size_t node_count = 0;
+	size_t count = 0;
 
-	for (; h != NULL; h = h->next, node_count++)
+	while (h)
 	{
-		printf("[%u] %s\n", h->str ? h->len : 0, h->str ? h->str : "(nil)");
+		if (h->str)
+		{
+			_putchar('[');
+			print_number(h->len);
+			_putchar(']');
+			_putchar(' ');
+			_puts(h->str);
+		}
+		else
+		{
+			_puts("[0] (nil)");
+		}
+		h = h->next;
+		count++;
 	}
-
-	return (node_count);
+	return (count);
 }
