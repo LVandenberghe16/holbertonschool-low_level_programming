87a4086f2de2afe5e22222dcbaecda41d7547bd7
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2024-12-02 13:17:14 +0100

    try without strlen

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -12,24 +12,23 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *temp;
+	size_t i;
 
 	if (!str)
 		return (NULL);
-
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
-
 	new_node->str = strdup(str);
 	if (!new_node->str)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
-	new_node->len = strlen(str);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	new_node->len = i;
 	new_node->next = NULL;
-
 	if (!*head)
 	{
 		*head = new_node;
@@ -41,6 +40,5 @@ list_t *add_node_end(list_t **head, const char *str)
 			temp = temp->next;
 		temp->next = new_node;
 	}
-
 	return (new_node);
 }
