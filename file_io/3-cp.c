972000fe33e79c1d5547d8884cf0e2a83cbb4984
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2024-12-04 21:48:30 +0100

    oui

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,21 +6,24 @@
 /**
  * main - Copies the content of a file to another file.
  * @argc: The number of arguments.
- * @argv: The arguments.
+ * @argv: The arguments (file_from and file_to).
  *
- * Return: 0 on success, exits with error codes on failure.
+ * Return: 0 on success, or exits with error codes on failure.
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, bytes_read, bytes_written;
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
 	char buffer[BUFFER_SIZE];
 
+	/* Check for correct number of arguments */
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
+	/* Open file_from */
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
@@ -28,7 +31,8 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	/* Open file_to */
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -36,6 +40,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
+	/* Read from file_from and write to file_to */
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
@@ -48,6 +53,7 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	/* Handle read errors */
 	if (bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
@@ -56,12 +62,12 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
+	/* Close file descriptors */
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-
 	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
