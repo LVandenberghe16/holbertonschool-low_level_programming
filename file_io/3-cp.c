f69c8a417f8423d569dd84980fb598b32e1c50b6
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2024-12-05 21:00:23 +0100

    oui

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,7 +1,82 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 1024
+
+/**
+ * handle_error - Prints an error message and exits the program.
+ * @message: The error message.
+ * @filename: The file causing the error (can be NULL).
+ * @exit_code: The exit code.
+ */
+void handle_error(const char *message, const char *filename, int exit_code)
+{
+	if (filename)
+		dprintf(STDERR_FILENO, "%s %s\n", message, filename);
+	else
+		dprintf(STDERR_FILENO, "%s\n", message);
+	exit(exit_code);
+}
+
+/**
+ * open_file - Opens a file with the given flags and mode.
+ * @filename: The name of the file.
+ * @flags: The flags for opening the file.
+ * @mode: The mode to use if the file is created.
+ *
+ * Return: The file descriptor.
+ */
+int open_file(const char *filename, int flags, mode_t mode)
+{
+	int fd = open(filename, flags, mode);
+
+	if (fd == -1)
+		handle_error("Error: Can't open file", filename, (flags & O_RDONLY) ?
+			98 : 99);
+
+	return (fd);
+}
+
+/**
+ * copy_content - Copies content from one file descriptor to another.
+ * @fd_from: The source file descriptor.
+ * @fd_to: The destination file descriptor.
+ * @file_from: The name of the source file (for error messages).
+ * @file_to: The name of the destination file (for error messages).
+ */
+void copy_content(int fd_from, int fd_to, const char *file_from,
+	const char *file_to)
+{
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read, bytes_written;
+
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+			handle_error("Error: Can't write to file", file_to, 99);
+	}
+
+	if (bytes_read == -1)
+		handle_error("Error: Can't read from file", file_from, 98);
+}
+
+/**
+ * close_file - Closes a file descriptor and handles errors.
+ * @fd: The file descriptor to close.
+ * @filename: The name of the file (for error messages).
+ */
+void close_file(int fd, const char *filename)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
 
 /**
  * main - Copies the content of a file to another file.
@@ -13,66 +88,17 @@
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
 
-	/* Check for correct number of arguments */
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		handle_error("Usage: cp file_from file_to", NULL, 97);
 
-	/* Open file_from */
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	fd_from = open_file(argv[1], O_RDONLY, 0);
+	fd_to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
-	/* Open file_to */
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
-		exit(99);
-	}
+	copy_content(fd_from, fd_to, argv[1], argv[2]);
 
-	/* Read from file_from and write to file_to */
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written != bytes_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			exit(99);
-		}
-	}
-
-	/* Handle read errors */
-	if (bytes_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
-		exit(98);
-	}
-
-	/* Close file descriptors */
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+	close_file(fd_from, argv[1]);
+	close_file(fd_to, argv[2]);
 
 	return (0);
 }
